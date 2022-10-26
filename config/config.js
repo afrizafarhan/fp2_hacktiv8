5d017c3b0db0b04639fdf9b4a60d5a7231a22ce9
farhan <farhanafriza24@gmail.com>
2022-10-26 23:54:29 +0700

    add port

--- config/config.js
+++ config/config.js
@@ -7,20 +7,23 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
-    dialect: "postgres"
+    dialect: "postgres",
+    port: process.env.DB_PORT
   },
   test: {
     username: process.env.DB_USERNAME_TEST,
     password: process.env.DB_PASSWORD_TEST,
     database: process.env.DB_DATABASE_TEST,
     host: process.env.DB_HOST_TEST,
-    dialect: "postgres"
+    dialect: "postgres",
+    port: process.env.DB_PORT
   },
   production: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
-    dialect: "postgres"
+    dialect: "postgres",
+    port: process.env.DB_PORT
   }
 }
